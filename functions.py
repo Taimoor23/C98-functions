26966225c6cb5ef394925dc7b3485e5d3501cc43
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-07-26 13:54:35 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,15 @@
+def SwapFileData():
+    file1=input("Enter your file name :  ")
+    file2=input("Enter your file name :  ")
+
+    with open(file1,'r') as sampleA:
+        data_sampleA=sampleA.read()
+
+    with open(fileTwoData, 'r') as sampleB:
+        data_sampleB=sampleB.read()
+
+    with open(fileOneData, 'w') as sampleA:
+        data_sampleA.write(data_sampleB)
+
+    with open(fileTwoData, 'w') as sampleB:
+        data_sampleB.write(data_sampleA)
