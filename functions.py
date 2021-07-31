fd456f680807585ebdd60624d8d98fdcd86fc77d
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2021-07-31 09:28:07 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,14 +5,14 @@ def SwapFileData():
     with open(file1,'r') as sampleA:
         data_sampleA=sampleA.read()
 
-    with open(fileTwoData, 'r') as sampleB:
+    with open(file2, 'r') as sampleB:
         data_sampleB=sampleB.read()
 
-    with open(fileOneData, 'w') as sampleA:
-        data_sampleA.write(data_sampleB)
+    with open(file1, 'w') as sampleA:
+       sampleA.write(data_sampleB)
 
-    with open(fileTwoData, 'w') as sampleB:
-        data_sampleB.write(data_sampleA)
+    with open(file2, 'w') as sampleB:
+       sampleB.write(data_sampleA)
 
         
 SwapFileData()        
